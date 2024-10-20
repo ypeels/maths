59db1b5b9cc19b63f07fcf7bf99285282a3fed6e
Jonathan E. Chen <ypeels@users.noreply.github.com>
2024-10-20 14:22:17 -0400

    widgets

--- foil.py
+++ foil.py
@@ -7,7 +7,7 @@ import sympy
 verbose = False
 
 
-def get_coeffs():
+def get_coeffs(probability_hard):
   '''returns 4 coefficients'''
   nonzero = list(range(-9, 0)) + list(range(1, 10)) # no 0 coeffs
   with_zero = list(range(-9, 10))
@@ -16,14 +16,14 @@ def get_coeffs():
     print(with_zero)
 
   # linear coefficient
-  if random.random() < 0.2:
+  if random.random() <= probability_hard:
     a, _ = random.choices(nonzero, k=2)
     d = 1
   else:
     a, d = 1, 1
 
   # constant term
-  if random.random() < 0.2:
+  if random.random() <= probability_hard:
     b, e = random.choices(with_zero, k=2)
   else:
     b, e = random.choices(nonzero, k=2) 
@@ -33,10 +33,11 @@ def get_coeffs():
 
 
 if __name__ == '__main__':
-  st.write("# FOIL")
-  num_problems = 20
+  st.write('# FOIL')
+  num_problems = st.slider('num problems', 1, 100, value=20)
+  probability_hard = st.slider('probability hard', 0, 1, value=0.2)
   for i in range(num_problems):
-    c = sympy.symbols('c')
+    x = sympy.symbols('x')
     a, b, d, e = get_coeffs()
-    st.write(f'{i+1}) ({a*c+b})({d*c+e}) = {sympy.expand((a*c + b)*(d*c + e))}')
+    st.write(f'{i+1}) ({a*x+b})({d*x+e}) = {sympy.expand((a*x + b)*(d*x + e))}')
   
